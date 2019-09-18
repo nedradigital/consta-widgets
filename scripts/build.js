0f55caa99aae3d908ae83efa2116519cc1b24b67
Sergey Golovin <golovim@gmail.com>
2019-09-18 14:35:35 +0300

    TSUB-96: Сборка библиотеки dashboard-constructor
    - добавлена возможность сборки библиотеки в dist
    - добалвен watch для сборки библиотеки, что позволит пересобирать изменения получать их при разработке другого проекта при помощи npm link (hot reload тоже будет работать)
    - наружу экспортим только widgets и dashboard

--- scripts/build.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk')
-
-const { build, spinner } = require('./tools')
-
-spinner.info(chalk.default.bgBlue('Enter build mode'))
-;(async () => {
-  await build()
-  spinner.succeed('Finished building')
-})()
