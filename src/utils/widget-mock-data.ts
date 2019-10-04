30a3f49dc95744490dddb29d4416c6f4aa39b5bd
evless <evless56@gmail.com>
2019-10-04 15:25:14 +0300

    GINF-10: Реализация виджета LinearChart

--- src/utils/widget-mock-data.ts
+++ src/utils/widget-mock-data.ts
@@ -111,6 +111,17 @@ const mockData: DataMap = {
       ],
     },
   ],
+  [DataType.LinearChart]: [
+    {
+      color: '#20B55F',
+      values: [{ x: 0, y: -1 }, { x: 1, y: 3 }, { x: 2, y: 1 }, { x: 3, y: 4 }],
+      dots: true,
+    },
+    {
+      color: '#56B9F2',
+      values: [{ x: 0, y: -2 }, { x: 1, y: 4 }, { x: 2, y: 0 }, { x: 3, y: 5 }],
+    },
+  ],
 }
 
 export function getWidgetMockData<T extends DataType>(dataType: T): DataMap[T] {
