7e22b7fcf3355dcb8fdee6986bc1e799e0d68a66
evless <evless56@gmail.com>
2019-11-05 16:31:21 +0300

    GINF-221: Пончик: Нет разделительных линий между цветами

--- src/components/DonutChart/index.tsx
+++ src/components/DonutChart/index.tsx
@@ -19,12 +19,20 @@ type Props = {
 const getSizeDonut = () => getCalculatedSize(16)
 const getPadding = () => getCalculatedSize(8)
 
+const getDonutRadius = (mainRadius: number, index: number) =>
+  mainRadius - (getSizeDonut() + getPadding()) * index
+
+const MIN_VISIBLE_GAP_WIDTH_RATIO = 0.0002
+const revertIndexes = [2, 1, 0] as const
+const getPadAngle = (mainRadius: number, index: number) => {
+  return getDonutRadius(mainRadius, revertIndexes[index]) * MIN_VISIBLE_GAP_WIDTH_RATIO
+}
+
 export const DonutChart: React.FC<Props> = ({ data = [], colorGroups }) => {
   const [ref, { width, height }, el] = useDimensions()
   const size = width && height ? Math.min(width, height) : 0
   const mainRadius = size / 2
   const sizeDonut = getSizeDonut()
-  const padding = getPadding()
 
   useLayoutEffect(() => {
     if (mainRadius) {
@@ -38,15 +46,18 @@ export const DonutChart: React.FC<Props> = ({ data = [], colorGroups }) => {
     <div ref={ref} className={css.main}>
       <svg width={size} height={size}>
         {data.slice(0, 3).map((d, index) => {
-          const radius = mainRadius - (sizeDonut + padding) * index
+          const outerRadius = getDonutRadius(mainRadius, index)
+          const innerRadius = outerRadius - sizeDonut
+          const padAngle = getPadAngle(mainRadius, index)
 
           return (
             <Donut
               key={index}
+              padAngle={padAngle}
               colorGroups={colorGroups}
               data={d}
-              innerRadius={radius - sizeDonut}
-              outerRadius={radius}
+              innerRadius={innerRadius}
+              outerRadius={outerRadius}
             />
           )
         })}
