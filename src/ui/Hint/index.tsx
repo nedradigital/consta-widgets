81980aa918fcdc873538099b9726fd3cea061647
Alexey Fyodorov <dev.fyodorov@gmail.com>
2019-11-19 21:19:12 +0300

    GINF-367: Общий механизм показа тултипов

--- src/ui/Hint/index.tsx
+++ src/ui/Hint/index.tsx
@@ -12,10 +12,21 @@ type Props = {
   direction?: Direction
   styles?: React.CSSProperties
   className?: string
+  containerRef?: React.Ref<HTMLDivElement>
 }
 
-export const Hint: React.FC<Props> = ({ children, direction = 'bottom', styles, className }) => (
-  <div className={classnames(css.hint, css[direction], className)} style={styles}>
+export const Hint: React.FC<Props> = ({
+  children,
+  direction = 'bottom',
+  styles,
+  className,
+  containerRef,
+}) => (
+  <div
+    ref={containerRef}
+    className={classnames(css.hint, css[direction], className)}
+    style={styles}
+  >
     {children}
   </div>
 )
