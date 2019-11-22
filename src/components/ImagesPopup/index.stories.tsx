5072250b776540cd901381c5526ac7b456b20591
Nikita Golubov <neekitkah@gmail.com>
2019-11-22 12:59:33 +0300

    GINF-328: Картинки: попап просмотра

--- src/components/ImagesPopup/index.stories.tsx
+++ src/components/ImagesPopup/index.stories.tsx
@@ -4,21 +4,20 @@ import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 import { withSmartKnobs } from 'storybook-addon-smart-knobs'
 
-import { blockCenteringDecorator } from '@/utils/Storybook'
+import { ImagesPopup } from '.'
 
-import { ImagesList } from '.'
-
-storiesOf('components/ImagesList', module)
+storiesOf('components/ImagesPopup', module)
   .addDecorator(withSmartKnobs())
-  .addDecorator(blockCenteringDecorator({ width: '20vw' }))
   .add('interactive', () => (
-    <ImagesList
+    <ImagesPopup
+      isVisible
       images={[
         { large: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg' },
         { preview: 'https://via.placeholder.com/150', large: 'https://via.placeholder.com/1500' },
         { large: 'https://via.placeholder.com/400x200' },
         { large: 'https://via.placeholder.com/200x400' },
       ]}
-      onClick={action('onClick')}
+      activeImage={1}
+      onRequestClose={action('onRequestClose')}
     />
   ))
