1200816c09ee5461ca13c0b438b1ed16c3a1dc25
Alexey Fyodorov <dev.fyodorov@gmail.com>
2019-12-04 12:37:31 +0300

    GINF-507: Подключить скрипт проверки путей файлов типизации на prepublish

--- types/amcharts-stub.d.ts
+++ types/amcharts-stub.d.ts
@@ -2,7 +2,6 @@ import { ExtendedFeatureCollection } from 'd3'
 
 // Перебиваем родные типы библиотеки для совместимости с d3
 // Через declare module перебить не удаётся: https://github.com/microsoft/TypeScript/issues/17042
-const content = {} as ExtendedFeatureCollection
+declare const content: ExtendedFeatureCollection
 
-// eslint-disable-next-line import/no-default-export
 export default content
