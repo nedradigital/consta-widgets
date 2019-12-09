809d7701c41cfe09bee02bf1bc8816b6c19ad839
evless <evless56@gmail.com>
2019-12-09 15:14:19 +0300

    GINF-201: Разобраться с тем, что если версии gaz-utils отличаются в dashboard-constructor и gaz-configs, то конструктор будет обращаться не к той версии пакета

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,44 @@
-const config = require('@gaz/configs/config/webpack/library.webpack.js')
-
 const { withCustomRules } = require('./helpers')
 
-module.exports = withCustomRules(config)
+const path = require('path')
+const webpackMerge = require('webpack-merge')
+const TerserPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const getCommonConfig = require('@gaz/configs/config/webpack/common.webpack')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const rootPath = process.env.ROOT_PATH
+const srcPath = path.resolve(rootPath, 'src')
+
+const libConfig = {
+  entry: [srcPath],
+  optimization: isProduction
+    ? {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            output: {
+              comments: false,
+            },
+          },
+        }),
+      ],
+    }
+    : undefined,
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(rootPath, 'lib'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    publicPath: './',
+  },
+  plugins: [
+    isProduction && new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([{ from: 'types', to: 'types' }])
+  ].filter(Boolean),
+}
+
+module.exports = withCustomRules(
+  webpackMerge(getCommonConfig({ isLibBuilding: true }), libConfig)
+)
