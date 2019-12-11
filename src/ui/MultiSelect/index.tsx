c7a6129f9cc30f5e1bcb78664d0f12784b987305
Maksim Andreev <andreev.kras@gmail.com>
2019-12-11 16:27:22 +0700

    GINF-542: Фильтрация: компонент мультиселекта

---
+++ src/ui/MultiSelect/index.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+
+import { useClickOutside } from '@gaz/utils/lib/use-click-outside'
+import { Checkbox } from '@gpn-design/uikit'
+import classnames from 'classnames'
+import * as _ from 'lodash'
+
+import { SelectedOption } from './components/SelectedOption'
+import css from './index.css'
+
+export type Option = {
+  id: string
+  name: string
+  initialValue?: boolean
+}
+
+type Values = readonly string[]
+
+export type Props = {
+  options: readonly Option[]
+  values: Values
+  onChange: (values: Values) => void
+  placeholder?: string
+}
+
+const toggleValuePresence = (values: Values, valueIdToToggle: string) =>
+  _.xor(values, [valueIdToToggle])
+
+export const MultiSelect: React.FC<Props> = ({
+  options,
+  values = [],
+  onChange,
+  placeholder = 'Выберите пункт',
+}) => {
+  const [isSelectOpened, setIsSelectOpened] = React.useState(false)
+  const handleOnSelectClick = () => setIsSelectOpened(!isSelectOpened)
+
+  const wrapperRef = React.useRef<HTMLDivElement>(null)
+  useClickOutside([wrapperRef], () => setIsSelectOpened(false))
+
+  const handleCheckboxChange = (id: string) => () => {
+    onChange(toggleValuePresence(values, id))
+  }
+
+  return (
+    <div className={css.multiSelect} ref={wrapperRef}>
+      <div
+        className={classnames(css.selectedList, isSelectOpened && css.isActive)}
+        onClick={handleOnSelectClick}
+      >
+        {values.length ? (
+          options.map(
+            option =>
+              values.includes(option.id) && (
+                <div className={css.selectedOptionWrapper} key={option.id}>
+                  <SelectedOption name={option.name} onRemove={handleCheckboxChange(option.id)} />
+                </div>
+              )
+          )
+        ) : (
+          <p className={css.placeholder}>{placeholder}</p>
+        )}
+        <div className={css.iconArrow} />
+      </div>
+
+      {isSelectOpened && (
+        <div className={css.optionsList}>
+          <div className={css.scrollableOptions}>
+            {options.map(option => (
+              <div
+                className={classnames(css.option, values.includes(option.id) && css.isSelected)}
+                key={option.id}
+              >
+                <Checkbox
+                  wpSize="m"
+                  className={css.optionCheckbox}
+                  onChange={handleCheckboxChange(option.id)}
+                  value={values.includes(option.id)}
+                >
+                  {option.name}
+                </Checkbox>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
