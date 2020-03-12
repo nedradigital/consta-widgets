d05ba2781d6f782fdc65e81b2d7802dacc7339a6
evless <evless56@gmail.com>
2020-03-12 17:10:37 +0300

    TRIVIAL: Поправил конфиг для генерации чейнджлогов. Поправил порядок, что бы генерация шла после публикации

--- .grenrc.js
+++ .grenrc.js
@@ -11,7 +11,7 @@ module.exports = {
   "groupBy": false,
   template: {
       issue: ({ text, url, name }) => (
-          `- ${name.replace(/((GINF|TSUB)-\d*)/, '[$1](https://jira.csssr.io/browse/$1)')} [${text}](${url})`
+          `- ${name.replace(/(([A-Z]+)-\d*)/, '[$1](https://jira.csssr.io/browse/$1)')} [${text}](${url})`
       ),
       changelogTitle: '### Изменения\n\n',
       release: '{{body}}'
