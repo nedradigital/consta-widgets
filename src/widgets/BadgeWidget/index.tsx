1ab9243e1380d1f28fd96f41bb8c6acfdf3fb19d
Nikita Golubov <neekitkah@gmail.com>
2020-04-29 17:13:47 +0300

    GDC-298: Бэдж. Неверный вид тултипа при недостатке места

--- src/widgets/BadgeWidget/index.tsx
+++ src/widgets/BadgeWidget/index.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 import { Text } from '@gpn-design/uikit'
 
-import { Tooltip } from '@/components/Tooltip'
+import { PositioningProps, Tooltip } from '@/components/Tooltip'
 import { WidgetSettingsCheckbox } from '@/components/WidgetSettingsCheckbox'
 import { WidgetSettingsSelect } from '@/components/WidgetSettingsSelect'
 import { DataMap, DataType } from '@/dashboard'
 import { badgeParams, BadgeParams as Params } from '@/dashboard/widget-params'
 import { Badge } from '@/ui/Badge'
+import { PositionState } from '@/utils/tooltips'
 import { widgetIdsByType } from '@/utils/widgets-list'
 import { createWidget, WidgetContentProps } from '@/utils/WidgetFactory'
 
@@ -27,13 +28,24 @@ export const BadgeWidgetContent: React.FC<WidgetContentProps<Data, Params>> = ({
 }) => {
   const { status, text, comment } = data
 
-  const ref = React.useRef(null)
-  const [tooltipVisible, setTooltipVisible] = React.useState(false)
+  const ref = React.useRef<HTMLDivElement>(null)
+  const [tooltipPosition, setTooltipPosition] = React.useState<PositionState>()
+
+  const handleMouseEnter: React.MouseEventHandler = e => {
+    const { top, left, width, height } = e.currentTarget.getBoundingClientRect()
+    setTooltipPosition({ x: left + width / 2, y: top + height / 2 })
+  }
+
+  const handleMouseLeave: React.MouseEventHandler = () => setTooltipPosition(undefined)
+
+  const tooltipPositioningProps: PositioningProps = isMinified
+    ? { position: tooltipPosition }
+    : { anchorRef: ref }
 
   return (
     <>
       {comment && (
-        <Tooltip isVisible={tooltipVisible} anchorRef={ref} direction="upCenter">
+        <Tooltip isVisible={!!tooltipPosition} direction="upCenter" {...tooltipPositioningProps}>
           <Text tag="div" weight="bold" transform="uppercase" spacing="xs" view="primary">
             Комментарий:
           </Text>
@@ -45,8 +57,8 @@ export const BadgeWidgetContent: React.FC<WidgetContentProps<Data, Params>> = ({
       <div
         ref={ref}
         className={css.main}
-        onMouseEnter={() => setTooltipVisible(true)}
-        onMouseLeave={() => setTooltipVisible(false)}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         <Badge
           wpSize={size}
