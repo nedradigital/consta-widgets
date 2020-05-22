b63898b47fe62a8a3fc851a1c9479ad87bd093d3
Nikita Golubov <neekitkah@gmail.com>
2020-05-22 15:14:41 +0300

    GDC-345: Статы. При нехватке места переносить длинный заголовок на вторую строку

--- src/Stats/index.stories.tsx
+++ src/Stats/index.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { number } from '@storybook/addon-knobs'
 import { withSmartKnobs } from 'storybook-addon-smart-knobs'
 
 import { blockCenteringDecorator, createMetadata, createStory } from '@/common/utils/Storybook'
@@ -19,6 +20,29 @@ export const Interactive = createStory(() => (
     layout="full"
   />
 ))
+
+export const WithLineBreak = createStory(
+  () => (
+    <div
+      style={{
+        width: number('Ширина контейнера', 200),
+      }}
+    >
+      <Stats
+        title="Сроки срочные сроки срочные сроки срочные"
+        value={217000}
+        badge={{
+          percentage: 2.1,
+          status: 'normal',
+        }}
+        unit="суток / час / суток / час / суток / час"
+        size="xs"
+        layout="full"
+      />
+    </div>
+  ),
+  { name: 'с переносом строки' }
+)
 
 export default createMetadata({
   title: 'components/Stats',
