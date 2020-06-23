019ad72c6dec2343a7d4202bb9208f54ba4a198d
evless <evless56@gmail.com>
2020-06-23 16:57:12 +0300

    GDC-517: CoreBarChart: подумать над тем как правильно задавать минимальный размер секции

--- src/core/BarChart/components/Group/__tests__/helpers.test.ts
+++ src/core/BarChart/components/Group/__tests__/helpers.test.ts
@@ -11,13 +11,13 @@ describe('getSections', () => {
     expect(result).toEqual([])
   })
 
-  it('', () => {
+  it('Получение секций колонки, если список секций пустой', () => {
     const result = getSections({ size: 100, sections: [], scaler })
 
     expect(result).toEqual([])
   })
 
-  it('', () => {
+  it('Получение секций колонки, с пустыми значениями в секциях', () => {
     const result = getSections({
       size: 100,
       sections: [
@@ -28,8 +28,32 @@ describe('getSections', () => {
     })
 
     expect(result).toEqual([
-      { color: 'red', value: 4, length: 4 },
+      { color: 'red', value: 4, length: 5 },
       { color: 'blue', value: undefined },
+    ])
+  })
+
+  it('Получение секций колонки, с укороченными длинными секциями', () => {
+    const result = getSections({
+      size: 100,
+      sections: [
+        { color: 'red', value: 90 },
+        { color: 'blue', value: 1 },
+        { color: 'yellow', value: 1 },
+        { color: 'aquamarine', value: 1 },
+        { color: 'black', value: 1 },
+        { color: 'orange', value: 6 },
+      ],
+      scaler,
+    })
+
+    expect(result).toEqual([
+      { color: 'red', value: 90, length: 75 },
+      { color: 'blue', value: 1, length: 5 },
+      { color: 'yellow', value: 1, length: 5 },
+      { color: 'aquamarine', value: 1, length: 5 },
+      { color: 'black', value: 1, length: 5 },
+      { color: 'orange', value: 6, length: 5 },
     ])
   })
 })
--- src/core/BarChart/components/Group/helpers.ts
+++ src/core/BarChart/components/Group/helpers.ts
@@ -1,5 +1,7 @@
 import { SectionItem } from '../Column'
 
+const MIN_SECTION_SIZE = 5
+
 export const getSections = ({
   size,
   sections,
@@ -9,7 +11,7 @@ export const getSections = ({
   sections?: readonly SectionItem[]
   scaler: (size: number, value: number) => number
 }) => {
-  return (sections || []).map(section => {
+  const elements = (sections || []).map(section => {
     if (section.value === undefined) {
       return section
     }
@@ -17,6 +19,37 @@ export const getSections = ({
     return {
       ...section,
       length: scaler(size, section.value),
+    }
+  })
+
+  const summarySize = elements.reduce((acc, item) => (item.length ? acc + item.length : acc), 0)
+  const countOfSmallElements = elements.reduce(
+    (acc, item) => (item.length === undefined || item.length > MIN_SECTION_SIZE ? acc : acc + 1),
+    0
+  )
+  const summarySizeOfBigElements = elements.reduce(
+    (acc, item) =>
+      item.length === undefined || item.length <= MIN_SECTION_SIZE ? acc : acc + item.length,
+    0
+  )
+
+  return elements.map(section => {
+    if (section.length === undefined) {
+      return section
+    }
+
+    if (section.length > MIN_SECTION_SIZE) {
+      return {
+        ...section,
+        length:
+          (section.length / summarySizeOfBigElements) *
+          (summarySize - countOfSmallElements * MIN_SECTION_SIZE),
+      }
+    }
+
+    return {
+      ...section,
+      length: MIN_SECTION_SIZE,
     }
   })
 }
