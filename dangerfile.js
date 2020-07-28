c0147b917567dcff2b49c383a7901292a3c7b6c4
evless <evless56@gmail.com>
2020-07-28 13:37:19 +0300

    GDC-637: Подумать над вариантами автоматизации проверок флоу работы с пр-ами

--- dangerfile.js
+++ dangerfile.js
@@ -1,26 +1,37 @@
 /* ⚠️
-* В этом файле можно импортить только из danger и стандартных библиотек ноды,
-* т.к. он выполняется через npx без установки всех пакетов
-* */
+ * В этом файле можно импортить только из danger и стандартных библиотек ноды,
+ * т.к. он выполняется через npx без установки всех пакетов
+ * */
 const { danger, fail, warn, markdown } = require('danger')
 
 const isValidCommit = commit => {
   return commit.message.length > 10
 }
 
-markdown(`Задача в Jira: ${danger.github.pr.head.ref}`)
-markdown(`Ссылка на стенд: http://${danger.github.pr.head.ref}.gpn-components.csssr.cloud`)
+const pr = danger.github.pr
+const reviews = danger.github.reviews
+const lastReview = reviews[reviews.length - 1]
 
-if (danger.github.pr.title.length < 10) {
+const wasReviewed = reviews.length >= 2 && new Date(lastReview.submitted_at) < new Date(pr.updated_at)
+
+markdown(`Задача в Jira: ${pr.head.ref}`)
+markdown(`Ссылка на стенд: http://${pr.head.ref}.gpn-components.csssr.cloud`)
+
+if (pr.title.length < 10) {
   fail('Заголовок PR должен быть осмысленным (длиной не менее 10 символов)')
 }
 
-if (!danger.github.pr.assignee) {
-  const log = danger.github.pr.draft ? warn : fail
+if (!pr.assignee) {
+  const log = pr.draft ? warn : fail
 
   log('Должен быть указан автор PR и ревьюеры')
 }
 
 if (!danger.git.commits.every(isValidCommit)) {
   fail('Сообщения к коммитам должны быть осмысленными (длиной не менее 10 символов)')
+}
+
+if (wasReviewed && pr.updated_at !== pr.created_at) {
+  warn('Если ведутся работы над PR, то нужно перевести его в draft')
+  warn('Если были внесены изменения после ревью, то нужно запросить ре-ревью')
 }
