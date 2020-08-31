f9c4e72dc858298c326ca17b3ccb05ac63ba7d23
Andrey Melnikov <andrey.melnikov@csssr.com>
2020-08-31 20:34:36 +0300

    GDC-732: Перейти на @testing-library/react: gpn-components

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,11 @@
 import T from 'react-test-renderer'
+import * as TL from '@testing-library/react'
+import { mocked as TM } from 'ts-jest/utils'
 
 import 'jest-extended'
 
 declare global {
   const TestRenderer: typeof T
+  const TestLibrary: typeof TL
+  const TestMocked: typeof TM
 }
