f5b7d75783bcdcdf8f4e9459ebd6b95a2cc85306
Ayaz Z <screamansqy@gmail.com>
2020-09-22 16:24:20 +0500

    GDC-760: BarChart. Возможность красить столбцы в разные цвета.

--- src/BarChart/__tests__/helpers.test.ts
+++ src/BarChart/__tests__/helpers.test.ts
@@ -12,7 +12,7 @@ describe('transformGroupsToCoreGroups', () => {
   ]
 
   it('преобразует BarChart группы к основным группам', () => {
-    const received = transformGroupsToCommonGroups(groups, ['red', 'blue', 'green'])
+    const received = transformGroupsToCommonGroups(groups, ['red', 'blue', 'green'], false)
 
     const expected: readonly CoreGroup[] = [
       {
@@ -27,6 +27,23 @@ describe('transformGroupsToCoreGroups', () => {
           { total: 0, sections: undefined },
           { total: 0, sections: undefined },
         ],
+      },
+    ]
+
+    expect(received).toEqual(expected)
+  })
+
+  it('преобразует BarChart группы к основным группам без пустых колонок при наличии соответствующего флага', () => {
+    const received = transformGroupsToCommonGroups(groups, ['red', 'blue', 'green'], true)
+
+    const expected: readonly CoreGroup[] = [
+      {
+        name: 'март',
+        columns: [
+          { total: 4, sections: [{ color: 'red', value: 4 }] },
+          { total: 2, sections: [{ color: 'blue', value: 2 }] },
+        ],
+        reversedColumns: [],
       },
     ]
 
--- src/BarChart/helpers.ts
+++ src/BarChart/helpers.ts
@@ -2,15 +2,21 @@ import { isNotNil } from '@consta/widgets-utils/lib/type-guards'
 
 import { Column, Group } from './'
 
-const defaultColumnItem = {
-  total: 0,
-  sections: undefined,
-}
+const getDefaultColumnItem = (isEmptyColumnsHidden: boolean) =>
+  isEmptyColumnsHidden
+    ? undefined
+    : {
+        total: 0,
+        sections: undefined,
+      }
 
-const getTransformColumn = (colors: readonly string[], filter: (value: number) => boolean) => (
-  column: Column,
-  index: number
-) => {
+const getTransformColumn = (
+  colors: readonly string[],
+  isEmptyColumnsHidden: boolean,
+  filter: (value: number) => boolean
+) => (column: Column, index: number) => {
+  const defaultColumnItem = getDefaultColumnItem(isEmptyColumnsHidden)
+
   if (!isNotNil(column)) {
     return defaultColumnItem
   }
@@ -25,16 +31,20 @@ const getTransformColumn = (colors: readonly string[], filter: (value: number) =
 
 export const transformGroupsToCommonGroups = (
   groups: readonly Group[],
-  colors: readonly string[]
+  colors: readonly string[],
+  isEmptyColumnsHidden: boolean
 ) => {
-  const getColumns = getTransformColumn(colors, v => v >= 0)
-  const getReversedColumns = getTransformColumn(colors, v => v < 0)
+  const getColumns = getTransformColumn(colors, isEmptyColumnsHidden, v => v >= 0)
+  const getReversedColumns = getTransformColumn(colors, isEmptyColumnsHidden, v => v < 0)
 
-  return groups.map(group => {
+  return groups.map(({ groupName, values }) => {
+    const columns = values.map(getColumns)
+    const reversedColumns = values.map(getReversedColumns)
+
     return {
-      name: group.groupName,
-      columns: group.values.map(getColumns),
-      reversedColumns: group.values.map(getReversedColumns),
+      name: groupName,
+      columns: isEmptyColumnsHidden ? columns.filter(Boolean) : columns,
+      reversedColumns: isEmptyColumnsHidden ? reversedColumns.filter(Boolean) : reversedColumns,
     }
   })
 }
