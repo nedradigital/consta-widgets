f94ce9df1eee4110eb88a2bb1398715a8a7ccecf
Golubov Nikita <neekitkah@gmail.com>
2020-10-07 13:52:38 +0300

    GDC-856: Виджеты: Обновить версию @consta/uikit

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,7 +9,6 @@ import { environmentDecorator, listOfThemes, ThemeDecorator } from '@/_private/s
 
 import stub from './stub.mdx'
 
-import '@consta/uikit/__internal__/src/utils/whitepaper/whitepaper.css'
 import '@consta/uikit/__internal__/src/components/Theme/Theme.css'
 import '@consta/uikit/__internal__/src/components/Theme/_color/Theme_color_gpnDisplay.css'
 import '@consta/uikit/__internal__/src/components/Theme/_color/Theme_color_gpnDark.css'
@@ -18,7 +17,6 @@ import '@consta/uikit/__internal__/src/components/Theme/_control/Theme_control_g
 import '@consta/uikit/__internal__/src/components/Theme/_font/Theme_font_gpnDefault.css'
 import '@consta/uikit/__internal__/src/components/Theme/_size/Theme_size_gpnDefault.css'
 import '@consta/uikit/__internal__/src/components/Theme/_space/Theme_space_gpnDefault.css'
-import '@consta/uikit/__internal__/src/components/Theme/_gap/Theme_gap_m.css'
 
 import './storybook.css'
 
