8f3a03d9f1cd00cbd567ad938e55962f9259fc30
Nikita Kamyshenko <nikita.kamyshenko@csssr.com>
2020-11-06 15:55:59 +0300

    GDC-398: Выбрать способ реализации отображения чисел над барами при группировке баров

--- src/BarChart/index.tsx
+++ src/BarChart/index.tsx
@@ -48,8 +48,6 @@ export const BarChart: React.FC<Props> = props => {
   } = props
 
   const commonGroups = transformGroupsToCommonGroups(groups, colors, isEmptyColumnsHidden)
-  const isMultiColumn = commonGroups.some(group => group.columns.length > 1)
-  const computedShowValues = showValues && (rest.isHorizontal || !isMultiColumn)
   const showReversed = isShowReversed({ groups: commonGroups, threshold: props.threshold })
   const groupsDomain = getGroupsDomain(commonGroups)
   const valuesDomain = getValuesDomain({
@@ -67,7 +65,7 @@ export const BarChart: React.FC<Props> = props => {
       valuesDomain={valuesDomain}
       maxColumn={maxColumn}
       size={size}
-      showValues={computedShowValues}
+      showValues={showValues}
       showReversed={showReversed}
       threshold={threshold}
       renderGroup={defaultRenderGroup}
